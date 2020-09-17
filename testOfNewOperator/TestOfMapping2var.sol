05301f4563f7b6c4acea42555a2d85fd113db0e1
Ivan Glagolev <70909614+Ivan-Glagolev@users.noreply.github.com>
2020-09-17 13:47:50 +0300

    Create TestOfMapping2var.sol
    Study of NEW operator for checking who has the majority par of votes. Without versions.

---
+++ testOfNewOperator/TestOfMapping2var.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.7.1;
+
+// SPDX-License-Identifier: IvanGlagolev
+
+contract LedgerBalance {
+   mapping(address => uint) public balances;
+
+   function updateBalance(uint newBalance) public {
+      balances[msg.sender] = newBalance;
+   }
+}
+contract Updater {
+   function updateBalance() public returns (uint) {
+      LedgerBalance ledgerBalance = new LedgerBalance();
+      ledgerBalance.updateBalance(10);
+      return ledgerBalance.balances(address(this));
+   }
+}
